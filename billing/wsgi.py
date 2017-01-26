6d9c8d04681f33cc5de3c46de50b01e5b954e97a
c-silber <csilber@ramapo.edu>
2017-01-25 20:55:41 -0500

    wsgi2

--- billing/wsgi.py
+++ billing/wsgi.py
@@ -15,5 +15,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "billing.settings")
 
 from dj_static import Cling
 
+application = get_wsgi_application()
 application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
